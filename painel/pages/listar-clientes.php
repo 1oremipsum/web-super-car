ee7516789d6044da5e93a880df6c7c025eed516a
Allan Sanches <allanterzisanches@gmail.com>
2023-11-02 21:39:59 -0300

    Implementado o sistema da barra de pesquisa dos clientes cadastrados

--- painel/pages/listar-clientes.php
+++ painel/pages/listar-clientes.php
@@ -1,11 +1,41 @@
 <?php 
     verificaPermissao(2);
-    $clientes = MySql::conectar()->prepare("SELECT * from `tb_site.clientes`");
-    $clientes->execute();
-    $clientes = $clientes->fetchAll();
 ?>
 <div class="box-content">
     <h2><i class="fa-solid fa-address-card"></i> Clientes Cadastrados</h2>
+
+    <div class="search-box">
+        <form method="post">
+           <input class="search-txt" type="text" name="busca" placeholder="Realizar pesquisa por nome ou e-mail">
+           <button class="search-btn" type="submit" name="acao">
+                <i class="fa-solid fa-magnifying-glass"></i>
+           </button>
+        </form>
+    </div><!-- search-box -->
+    <div class="clear"></div>
+
+    <?php 
+        $query = "";
+        if(isset($_POST['acao'])){
+            $busca = $_POST['busca'];
+            $query = " WHERE nome LIKE '%$busca%' OR email LIKE '%$busca%'";
+        }
+        $clientes = MySql::conectar()->prepare("SELECT * from `tb_site.clientes` $query");
+        $clientes->execute();
+        $clientes = $clientes->fetchAll();
+
+        if(isset($_POST['acao'])){
+            $resultados = count($clientes);
+            if($resultados == 1){
+                echo '<div class="busca-result"><p>Apenas <b>'.$resultados.'</b> resultado encontrado.</p></div>';
+            }else if ($resultados > 1) {
+                echo '<div class="busca-result"><p>Sua pesquisa encontrou <b>'.$resultados.'</b> resultados.</p></div>';
+            }else {
+                echo '<div class="busca-result"><p>Não encontramos nenhum <b>cliente</b> com <b>nome ou e-mail</b> semelhante a <b>"'.$_POST['busca'].'"</b>.</p></div>';
+            }
+        }
+    ?>
+
     <div class="boxes">
         <?php foreach ($clientes as $key => $value) { ?>
 
